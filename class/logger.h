14f79cc522fefe74fa3599c34e41652a113f0075
Zhizhong Yan <A2564011261@163.com>
2024-07-21 16:47:02 +0800

    Update logger.h

--- class/logger.h
+++ class/logger.h
@@ -3,3 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+
+File openAFile() {
+    File *fp = fopen("log.log", "a");
+    if(!fp) fp = fopen("log.log", "w");
+    return fp;
+}
+
+void printLogIntoFile(char *type, char *context, File *fp) {
+    
+}
