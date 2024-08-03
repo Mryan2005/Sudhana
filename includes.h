7d6464b7aaa097a895b1a6ddcfb4004de1b4c1e4
Zhizhong Yan <A2564011261@163.com>
2024-08-03 23:17:57 +0800

    add(includes.h): add it

---
+++ includes.h
@@ -0,0 +1,20 @@
+//
+// Created by A2564 on 24-8-3.
+//
+
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <windows.h>
+#include "class/linkList.h"
+#include "class/element.h"
+#include "class/logger.h"
+#include "class/graph.h"
+#include "class/stack.h"
+#include "class/calculator.h"
+
+#endif //INCLUDES_H
